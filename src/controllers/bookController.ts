52f9737af7f7ff77fc00609c871dee1746697c91
fnpratto <fnpratto@gmail.com>
2024-10-28 15:01:42 -0300

    Adding endpoints for searching books

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -36,9 +36,89 @@ export const getBookById = async (req: Request, res: Response) => {
   }
 };
 
+export const getBookByAuthor = async (req: Request, res: Response) => {
+  const { author } = req.params;
+
+  try {
+    const book = await Book.findOne({ where: { author } });
+
+    if (!book) {
+      res.status(404).json({ message: "Book not found" });
+      return;
+    }
+    res.json({
+      title: book.title,
+      author: book.author,
+      coverimage: book.coverimage || "default-cover.jpg",
+      publication_date: book.publication_date,
+      genre: book.genre,
+      summary: book.summary || "none",
+      numberreviews: book.numberreviews || 0,
+      averagerating: book.averagerating,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching book data", error });
+  }
+};
+
+export const getBookByName = async (req: Request, res: Response) => {
+  const { title } = req.params;
+
+  try {
+    const book = await Book.findOne({ where: { title } });
+
+    if (!book) {
+      res.status(404).json({ message: "Book not found" });
+      return;
+    }
+    res.json({
+      title: book.title,
+      author: book.author,
+      coverimage: book.coverimage || "default-cover.jpg",
+      publication_date: book.publication_date,
+      genre: book.genre,
+      summary: book.summary || "none",
+      numberreviews: book.numberreviews || 0,
+      averagerating: book.averagerating,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching book data", error });
+  }
+};
+
+export const getBookByTitleAndAuthor = async (req: Request, res: Response) => {
+  const { title, author } = req.params;
+
+  try {
+    const book = await Book.findOne({ where: { title, author } });
+
+    if (!book) {
+      res.status(404).json({ message: "Book not found" });
+      return;
+    }
+
+    res.json({
+      title: book.title,
+      author: book.author,
+      coverimage: book.coverimage || "default-cover.jpg",
+      publication_date: book.publication_date,
+      genre: book.genre,
+      summary: book.summary || "none",
+      numberreviews: book.numberreviews || 0,
+      averagerating: book.averagerating,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching book data", error });
+  }
+};
+
+
 
 export const createBook = async (req: Request, res: Response) => {
-  const { id,title, author, coverimage,publication_date, genre, summary, averagerating,numberreviews } = req.body;
+  const { id, title, author, coverimage, publication_date, genre, summary, averagerating, numberreviews } = req.body;
 
   if (!id || !title || !author || !publication_date || !genre || !summary) {
     res.status(400).json({ message: "Title, author, publication date, genre, and summary are required." });
@@ -54,9 +134,9 @@ export const createBook = async (req: Request, res: Response) => {
       publication_date,
       genre,
       summary,
-      averagerating: averagerating || 0.0, 
+      averagerating: averagerating || 0.0,
       numberreviews: numberreviews || 0,
-      
+
     })
 
     res.status(201).json(newBook);
--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
-import { getBooks, 
-        createBook, 
-        getBookById 
-    } from "../controllers/bookController";
+import {
+    getBooks,
+    createBook,
+    getBookById,
+    getBookByTitleAndAuthor,
+    getBookByAuthor,
+    getBookByName
+} from "../controllers/bookController";
 
 const router = Router();
 
 router.get("/", getBooks);
 router.get("/:id", getBookById);
+router.get('/:title/:author', getBookByTitleAndAuthor);
+router.get('/:title', getBookByName);
+router.get('/:author', getBookByAuthor);
 router.post("/", createBook);
 
 export default router;
