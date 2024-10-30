8c3cccfe0168af7e601a881ae2d6559ffb704496
Lucas <lgrati@fi.uba.ar>
2024-10-30 17:39:25 -0300

    allow cors delete

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PATCH", "OPTIONS", "PATCH"],
+    methods: ["GET", "POST", "PATCH", "OPTIONS", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
